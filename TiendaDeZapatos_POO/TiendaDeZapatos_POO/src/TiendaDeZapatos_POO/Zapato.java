fe3d1c7cd7dd9243cef33d3d5cf31e23e6cbd248
Juan Dario Rodas <jdrodas@hotmail.com>
2023-01-23 19:03:39 -0500

    Agregado el programa inicial de TiendaDeZapatos_POO

---
+++ TiendaDeZapatos_POO/TiendaDeZapatos_POO/src/TiendaDeZapatos_POO/Zapato.java
@@ -0,0 +1,41 @@
+package TiendaDeZapatos_POO;
+
+public class Zapato {
+
+    //Zona de Atributos
+    private String estilo, color;
+    private int talla;
+
+    //Zona de Constructor
+    public Zapato(){
+        talla = 0;
+        estilo = "ninguno";
+        color = "Sin asignar";
+    }
+
+    public void setTalla(int datoTalla) {
+        if (datoTalla >= 28 && datoTalla <= 44)
+            talla = datoTalla;
+        else
+            talla = 0;
+    }
+    public int getTalla() {
+        return talla;
+    }
+
+    public void setEstilo(String datoEstilo) {
+        estilo = datoEstilo;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setColor(String datoColor) {
+        color = datoColor;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
